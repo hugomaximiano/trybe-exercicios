332724d8896eeda30cb88f1279923a2aeed48ea8
Hugo Maximiano <hugomaximiano@hotmail.com>
2021-09-02 15:53:52 -0300

    exercicio introdução

---
+++ Fundamentos/bloco-5-DOM-eventos-webStorage/script.js
@@ -0,0 +1,3 @@
+document.getElementById("header-container").innerText = "The Matrix";
+document.getElementById("terceiro-p").innerText = "Considerado por muitos como o melhor filme de ficção dos ultimos 30 anos";
+document.getElementById("segundo-p").innerText = "Um filme de baixo orçamento que surprendeu a todos";
